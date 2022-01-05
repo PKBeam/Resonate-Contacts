c5cb95fc52ad999a2553a02c4cbf07999de3c226
Vincent Liu <pkchu99@gmail.com>
2022-01-06 03:39:39 +1100

    more scrolling issues fixed; add colours to names

--- src/AvatarText.js
+++ src/AvatarText.js
@@ -1,6 +1,18 @@
 import { Card, ListGroup } from 'react-bootstrap';
 import './AvatarText.css';
 
+// generate a random-ish color for a name
+function colorForName(name) {
+  var c = 1;
+  for (var i = 0; i < name.length; i++) {
+    c *= name.charCodeAt(i);
+  }
+  var maxCol = parseInt("FFFFFF", 16)
+  var colString = (c % maxCol).toString(16)
+  var leftPadded = ("0000" + colString).slice(-6)
+  return "#" + leftPadded
+}
+
 function AvatarText(props) {
 
   var split = props.name.split(" ")
@@ -8,7 +20,7 @@ function AvatarText(props) {
   var lastInitial = split[split.length - 1][0]
   return (
     <div className="list-avatar">
-      <div className="avatar">
+      <div className="avatar" style={{backgroundColor: colorForName(props.name)}}>
           <svg className="square" style={{width: props.width + "px"}} viewBox='0 0 1 1'></svg>
           <div className="avatar-text">
               {firstInitial + lastInitial}
